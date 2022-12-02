896f8fbc987ada97ad9d66f64e34361731abd282
이동건 <nerw173@gmail.com>
2022-12-03 01:12:30 +0900

    [ Update ] : Google One Tap Login 적용

--- feature/login/src/main/java/com/yapp/gallery/login/LoginScreen.kt
+++ feature/login/src/main/java/com/yapp/gallery/login/LoginScreen.kt
@@ -1,25 +1,27 @@
 package com.yapp.gallery.login
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
 fun LoginScreen(
     googleLogin : () -> Unit
 ){
-    Scaffold { padding ->
+    Scaffold(
+        modifier = Modifier.fillMaxSize()
+    ) { padding ->
         Column(
             modifier = Modifier
                 .padding(padding)
-                .fillMaxSize()
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(text = "로그인 테스트")
             Button(onClick = googleLogin) {
