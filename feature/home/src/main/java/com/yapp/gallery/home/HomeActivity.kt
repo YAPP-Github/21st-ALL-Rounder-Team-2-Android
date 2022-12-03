95ad91b46a909f25406d47b70b09d2b1dcec9599
이동건 <nerw173@gmail.com>
2022-12-03 18:55:55 +0900

    [ Feature ] : Hilt 적용

---
+++ feature/home/src/main/java/com/yapp/gallery/home/HomeActivity.kt
@@ -0,0 +1,16 @@
+package com.yapp.gallery.home
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HomeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HomeScreen()
+        }
+    }
+}
