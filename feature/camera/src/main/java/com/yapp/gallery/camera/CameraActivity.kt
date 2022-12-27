63952b3e0c6ef3ec0fbd5a77255f808d3220ee22
taeseongyun <yts0646@gmail.com>
2022-12-27 22:13:13 +0900

    [ Feature ] : 카메라 이동 로직 구현 완료

--- feature/camera/src/main/java/com/yapp/gallery/camera/CameraActivity.kt
+++ feature/camera/src/main/java/com/yapp/gallery/camera/CameraActivity.kt
@@ -7,29 +7,38 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.yapp.gallery.camera.databinding.ActivityCameraBinding
 import com.yapp.gallery.common.util.onCheckPermissions
+import com.yapp.navigator.saver.SaverNavigator
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class CameraActivity : AppCompatActivity() {
 
+    @Inject
+    lateinit var saverNavigator: SaverNavigator
     private lateinit var binding: ActivityCameraBinding
 
-    private val permissionLaunch = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGrant ->
-        if(isGrant) {
-            binding.composeView.setContent {
-                CameraView(
-                    onImageCapture = {},
-                    onDismiss = { finish() }
-                )
+    private val permissionLaunch =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGrant ->
+            if (isGrant) {
+                binding.composeView.setContent {
+                    CameraView(
+                        onImageCapture = { startActivity(saverNavigator.intentTo(this, it)) },
+                        onDismiss = { finish() }
+                    )
+                }
             }
         }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = DataBindingUtil.setContentView<ActivityCameraBinding>(this, R.layout.activity_camera).also {
-            it.lifecycleOwner = this
-            it.executePendingBindings()
-        }
+        binding =
+            DataBindingUtil.setContentView<ActivityCameraBinding>(this, R.layout.activity_camera)
+                .also {
+                    it.lifecycleOwner = this
+                    it.executePendingBindings()
+                }
 
         onCheckPermissions(activity = this, permission = Manifest.permission.CAMERA, onGrant = {
             permissionLaunch.launch(Manifest.permission.CAMERA)
