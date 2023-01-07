96f9cce60ce9b3aadd04c0d89557c729969950e9
taeseongyun <yts0646@gmail.com>
2023-01-07 22:28:03 +0900

    [ Feature ] : Saver 액티비티 구현 완료

--- feature/saver/src/main/java/com/yapp/gallery/saver/SaverActivity.kt
+++ feature/saver/src/main/java/com/yapp/gallery/saver/SaverActivity.kt
@@ -1,8 +1,8 @@
 package com.yapp.gallery.saver
 
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.yapp.gallery.saver.databinding.ActivitySaverBinding
 
@@ -13,16 +13,31 @@ class SaverActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = DataBindingUtil.setContentView<ActivitySaverBinding>(this, R.layout.activity_saver)
-            .also {
-                it.lifecycleOwner = this
-                it.executePendingBindings()
-            }
+        binding =
+            DataBindingUtil.setContentView<ActivitySaverBinding>(this, R.layout.activity_saver)
+                .also {
+                    it.lifecycleOwner = this
+                    it.executePendingBindings()
+                }
 
         val uri = intent.getParcelableExtra("uri") as? Uri
 
         binding.composeView.setContent {
-            SaverView(uri = uri!!)
+            uri?.let {
+                SaverView(
+                    uri = it,
+                    saveToFile = {
+                        /**
+                         * 해당 함수 호출부에서 Dialog를 띄워주시면 됩니다 !
+                         * finishAffinity -> https://velog.io/@windsekirun/Android-Exit-Applicaction-with-finishAffinity
+                         *
+                         * finishAffinity 는 동일 테스크에 존재하는 액티비티를 닫아버리는거라 CameraActivity 는 다른 테스트로
+                         * 호출 하셔야합니다 ~
+                         **/
+                        finishAffinity()
+                    }
+                )
+            }
         }
     }
 }
--- feature/saver/src/main/java/com/yapp/gallery/saver/SaverView.kt
+++ feature/saver/src/main/java/com/yapp/gallery/saver/SaverView.kt
@@ -2,10 +2,57 @@ package com.yapp.gallery.saver
 
 import android.net.Uri
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import coil.compose.rememberAsyncImagePainter
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 
 @Composable
-fun SaverView(uri: Uri) {
-    Image(painter = rememberAsyncImagePainter(uri), contentDescription = null)
+fun SaverView(
+    saveToFile: (Uri) -> Unit,
+    uri: Uri
+) {
+
+    Box(modifier = Modifier
+        .background(Color.Black)
+        .fillMaxSize()) {
+
+        Image(
+            painter = rememberImagePainter(uri),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize()
+        )
+        
+        Row(modifier = Modifier
+            .align(Alignment.BottomCenter)
+            .padding(bottom = 35.dp)
+            .fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            Button(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(start = 16.dp)
+                ,
+                onClick = { /*TODO*/ }
+            ) {
+                Text(text = stringResource(id = R.string.saver_continuation))
+            }
+            Button(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(end = 16.dp),
+                onClick = { saveToFile(uri) }
+            ) {
+                Text(text = stringResource(id = R.string.saver_save_to_file))
+            }
+        }
+    }
 }
