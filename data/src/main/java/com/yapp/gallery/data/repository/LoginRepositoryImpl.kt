64ca38ac2d8de0733257e03eba75514b81e349d1
이동건 <nerw173@gmail.com>
2023-01-07 20:45:30 +0900

    [ Feature ] : AuthInterceptor 추가

--- data/src/main/java/com/yapp/gallery/data/repository/LoginRepositoryImpl.kt
+++ data/src/main/java/com/yapp/gallery/data/repository/LoginRepositoryImpl.kt
@@ -11,7 +11,7 @@ class LoginRepositoryImpl @Inject constructor(
         return loginRemoteDataSource.tokenLogin(accessToken).firebase_token
     }
 
-    override suspend fun createUser(idToken: String, firebaseUserId: String): Long {
-        return loginRemoteDataSource.createUser(idToken, firebaseUserId).id
+    override suspend fun createUser(firebaseUserId: String): Long {
+        return loginRemoteDataSource.createUser(firebaseUserId).id
     }
 }
