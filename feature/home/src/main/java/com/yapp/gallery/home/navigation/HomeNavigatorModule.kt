149e1ee83606b470df0db6733771daefc00226e6
이동건 <nerw173@gmail.com>
2023-01-10 14:30:59 +0900

    [ ADD ] : Javascript Interface 임시

--- feature/home/src/main/java/com/yapp/gallery/home/navigation/HomeNavigatorModule.kt
+++ feature/home/src/main/java/com/yapp/gallery/home/navigation/HomeNavigatorModule.kt
@@ -1,17 +1,15 @@
-package com.yapp.gallery.di
+package com.yapp.gallery.home.navigation
 
-import com.yapp.gallery.home.navigation.HomeNavigatorImpl
 import com.yapp.gallery.navigation.home.HomeNavigator
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class NavigationModule {
+abstract class HomeNavigatorModule {
     @Binds
     @Singleton
     abstract fun bindHomeNavigator(navigatorImpl: HomeNavigatorImpl) : HomeNavigator
