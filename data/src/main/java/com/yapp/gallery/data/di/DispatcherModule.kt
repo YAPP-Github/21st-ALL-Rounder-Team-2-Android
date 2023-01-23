e2c9e4ec15412f73bbb216fa9adca5db305553f6
이동건 <nerw173@gmail.com>
2023-01-23 16:32:36 +0900

    [ Feature ] : Dispatcher 모듈 추가

---
+++ data/src/main/java/com/yapp/gallery/data/di/DispatcherModule.kt
@@ -0,0 +1,35 @@
+package com.yapp.gallery.data.di
+
+import dagger.Module
+import dagger.Provides
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Qualifier
+
+@Module
+object DispatcherModule {
+    @Retention(AnnotationRetention.BINARY)
+    @Qualifier
+    annotation class DefaultDispatcher
+
+    @Retention(AnnotationRetention.BINARY)
+    @Qualifier
+    annotation class IoDispatcher
+
+    @Retention(AnnotationRetention.BINARY)
+    @Qualifier
+    annotation class MainDispatcher
+
+
+    @DefaultDispatcher
+    @Provides
+    fun providesDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+
+    @IoDispatcher
+    @Provides
+    fun providesIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @MainDispatcher
+    @Provides
+    fun providesMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
+}
