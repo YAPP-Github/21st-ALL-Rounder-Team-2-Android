53dd2daebb44e284c7167c9e9c0fadf0798cb6ae
TaeseongYun <yts0646@gmail.com>
2023-02-18 22:35:27 +0900

    [ Feature ] : 카메라 화면구현 및 저장 로직 구현 (#22)
    * [ Refactor ] : 카메라 액티비티 수정
    
    * [ Feature ] : 사진 저장 액티비티 및 다이얼로그 추가 완료
    
    * [ Refactor, chore ] : 퍼미션 로직 수정 및 fragment , material 라이브러리 추가
    
    * [ Feature ] : 이미지 피커 라이브러리 추가
    
    * [ Feature ] : saver navigation 로직 변경 ( uris 매개변수 추가 )
    
    * [ Feature ] : 앨범 노출 및 저장로직 추가
    
    * [ Feature ] : 매개변수 리스트성 Uri 추가로 인해 로직 수정

--- feature/home/src/main/java/com/yapp/gallery/home/screen/home/HomeActivity.kt
+++ feature/home/src/main/java/com/yapp/gallery/home/screen/home/HomeActivity.kt
@@ -1,15 +1,19 @@
 package com.yapp.gallery.home.screen.home
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.nguyenhoanglam.imagepicker.model.ImagePickerConfig
+import com.nguyenhoanglam.imagepicker.ui.imagepicker.registerImagePicker
 import com.yapp.gallery.common.theme.GalleryTheme
 import com.yapp.gallery.home.navigation.HomeNavHost
 import com.yapp.gallery.navigation.profile.ProfileNavigator
 import com.yapp.navigation.camera.CameraNavigator
+import com.yapp.navigator.saver.SaverNavigator
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -17,9 +21,16 @@ import javax.inject.Inject
 class HomeActivity : ComponentActivity() {
     @Inject lateinit var cameraNavigator: CameraNavigator
     @Inject lateinit var profileNavigator: ProfileNavigator
+    @Inject lateinit var saverNavigator: SaverNavigator
     private lateinit var navController : NavHostController
 
     private var backKeyPressedTime: Long = 0
+
+    private val imagePicker = registerImagePicker {
+        if(it.isNotEmpty()) {
+            startActivity(saverNavigator.intentTo(context = this, uris = it.map { image -> image.uri }))
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +38,16 @@ class HomeActivity : ComponentActivity() {
             navController = rememberNavController()
             GalleryTheme {
                 HomeNavHost(navHostController = navController, profileNavigator = profileNavigator,
-                    cameraNavigator = cameraNavigator
+                    cameraNavigator = cameraNavigator, navToImagePicker = {
+                        imagePicker.launch(
+                            ImagePickerConfig(
+                                isMultipleMode = true,
+                                maxSize = 5,
+                                doneTitle = "완료",
+                                limitMessage = "사진은 최대 5장까지 선택 가능해요!"
+                            )
+                        )
+                    }
                 )
             }
         }
--- feature/saver/src/main/java/com/yapp/gallery/saver/SaverActivity.kt
+++ feature/saver/src/main/java/com/yapp/gallery/saver/SaverActivity.kt
@@ -2,8 +2,10 @@ package com.yapp.gallery.saver
 
 import android.net.Uri
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.yapp.gallery.common.theme.GalleryTheme
 import com.yapp.gallery.saver.databinding.ActivitySaverBinding
 
 class SaverActivity : AppCompatActivity() {
@@ -21,22 +23,29 @@ class SaverActivity : AppCompatActivity() {
                 }
 
         val uri = intent.getParcelableExtra("uri") as? Uri
+        val uris = intent.getParcelableArrayExtra("uris")
 
         binding.composeView.setContent {
+            if(!uris.isNullOrEmpty()) {
+                GalleryTheme {
+                    SaverView(onRetryListener = {
+                        finish()
+                    }, saveToFile = {
+                        SaverDialog.show(supportFragmentManager)
+                    }, uris= uris.filterIsInstance<Uri>())
+                }
+            }
+
             uri?.let {
-                SaverView(
-                    uri = it,
-                    saveToFile = {
-                        /**
-                         * 해당 함수 호출부에서 Dialog를 띄워주시면 됩니다 !
-                         * finishAffinity -> https://velog.io/@windsekirun/Android-Exit-Applicaction-with-finishAffinity
-                         *
-                         * finishAffinity 는 동일 테스크에 존재하는 액티비티를 닫아버리는거라 CameraActivity 는 다른 테스트로
-                         * 호출 하셔야합니다 ~
-                         **/
-                        finishAffinity()
-                    }
-                )
+                GalleryTheme {
+                    SaverView(
+                        uri = it,
+                        onRetryListener = { finish() },
+                        saveToFile = {
+                            SaverDialog.show(supportFragmentManager)
+                        }
+                    )
+                }
             }
         }
     }
